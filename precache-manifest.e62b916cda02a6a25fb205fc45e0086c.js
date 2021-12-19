450cb07fc8cd75c7f0c725cfa2209200d4f93322
Ankit Bhujeja <bhujejaankit@gmail.com>
2021-12-19 14:49:40 +0530

    Updates

---
+++ precache-manifest.e62b916cda02a6a25fb205fc45e0086c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd63ce57feb700ac4091b793004c3af",
+    "url": "/box-office-lighttheme/index.html"
+  },
+  {
+    "revision": "ab8033563117c16065d3",
+    "url": "/box-office-lighttheme/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "1896efd86141ea92109c",
+    "url": "/box-office-lighttheme/static/js/2.fc99998d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office-lighttheme/static/js/2.fc99998d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8033563117c16065d3",
+    "url": "/box-office-lighttheme/static/js/main.59d5a3fa.chunk.js"
+  },
+  {
+    "revision": "f264a3023286d27e30f6",
+    "url": "/box-office-lighttheme/static/js/runtime-main.310d1e1f.js"
+  }
+]);
